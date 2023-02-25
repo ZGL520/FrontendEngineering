228e47b161f4b7a0dfc663271cf8e3998080d7d1
zgl <zglin1024@gmail.com>
2023-02-25 19:33:59 +0800

    feat: 添加clean-webpack-plugin

--- enginieering_module/webpack.config.js
+++ enginieering_module/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -45,4 +46,8 @@ module.exports = {
       }
     ]
   },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
 };
